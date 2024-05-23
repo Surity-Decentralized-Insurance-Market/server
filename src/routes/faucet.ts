fad83316dcb4c19722646ae897ad638d2f8a65a9
Spandan Barve <spandan567@gmail.com>
2024-05-23 12:34:44 +0530

    bugfix

--- src/routes/faucet.ts
+++ src/routes/faucet.ts
@@ -10,16 +10,16 @@ router.get("/request/:address", async (req, res) => {
     await FaucetContract.request(address);
     res.status(200).send({ message: "success" });
   } catch (err: any) {
-    res.status(500).send({ message: err.message.reason || err.message | err });
+    res.status(500).send({ error: err.message.reason || err.message | err });
   }
 });
 
 router.get("/balance", async (req, res) => {
   try {
     const balance = await FaucetContract.balance();
-    res.send({ balance });
+    res.send({ balance: Number(balance) });
   } catch (err: any) {
-    res.status(500).send({ message: err.message.reason || err.message | err });
+    res.status(500).send({ error: err.message.reason || err.message | err });
   }
 });
 
@@ -27,18 +27,21 @@ router.get("/configuration", async (req, res) => {
   try {
     const defaultTimeout = await FaucetContract.defaultTimeout();
     const tokensPerRequest = await FaucetContract.tokensPerRequest();
-    res.send({ defaultTimeout, tokensPerRequest });
+    res.send({
+      defaultTimeout: Number(defaultTimeout),
+      tokensPerRequest: Number(tokensPerRequest),
+    });
   } catch (err: any) {
-    res.status(500).send({ message: err.message.reason || err.message | err });
+    res.status(500).send({ error: err.message.reason || err.message | err });
   }
 });
 
 router.get("/next-request-time/:address", async (req, res) => {
   try {
     const nextTime = await FaucetContract.nextRequestTime(req.params.address);
-    res.send({ time: nextTime });
+    res.send({ time: Number(nextTime) });
   } catch (err: any) {
-    res.status(500).send({ message: err.message.reason || err.message | err });
+    res.status(500).send({ error: err.message.reason || err.message | err });
   }
 });
 
